6441114ea526923eb2b9fba559bc0dae1144787e
dabrabgellak <barbaracalle10i3@gmail.com>
2019-06-21 02:07:47 +0000

    alphabet with the \n

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int bets;
+
+	for (bets = 'a'; bets <= 'z'; bets++)
+	putchar(bets);
+	putchar('\n');
+return (0);
+}
