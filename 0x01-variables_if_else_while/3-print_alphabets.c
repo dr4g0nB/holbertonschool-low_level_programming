933a2cd7eb31a6b08b2208ff9f83b3628603099d
dabrabgellak <barbaracalle10i3@gmail.com>
2019-06-21 14:38:22 +0000

    corrected the for statement

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int bets;
+	int alpha;
+
+	for (bets = 'a'; bets <= 'z'; bets++)
+	{
+	putchar(bets);
+	}
+	for (alpha = 'A'; alpha <= 'Z'; alpha++){
+	putchar(alpha);
+	}
+	putchar('\n');
+return (0);
+}
