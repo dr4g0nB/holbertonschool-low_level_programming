301402bdd7c8b67a68c2402357c22f2a79cde143
dabrabgellak <barbaracalle10i3@gmail.com>
2019-06-24 21:18:52 +0000

    The result with two loops, for and if

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,14 @@
+#include <holberton.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	_putchar("Holberton"\n);
+	return (0);
+}
