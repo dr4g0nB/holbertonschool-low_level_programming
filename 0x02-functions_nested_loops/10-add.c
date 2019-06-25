86d646eaff9fd025c7e4cb527fb9abbd26bc88cf
dabrabgellak <barbaracalle10i3@gmail.com>
2019-06-25 22:07:47 +0000

    added the description

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * add - check the code for Holberton School students.
+ * @n1: number 1.
+ * @n2: number 2.
+ * Return: n1 & n2.
  */
 
 int add(int n1, int n2)
