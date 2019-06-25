d40eb8a39cdc274a70b6dc2566b180a48ccbd250
dabrabgellak <barbaracalle10i3@gmail.com>
2019-06-25 16:18:29 +0000

    prints 10 times with two for loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,10 @@
 void print_alphabet_x10(void)
 {
 	char alpha = 97;
-	int sn = 1;	
+	int sn = 1;
 
-	for(sn = 1; sn <= 10; sn++){
+	for (sn = 1; sn <= 10; sn++)
+	{
 		for (alpha = 97; alpha <= 122; alpha++)
 		{
 			_putchar(alpha);
