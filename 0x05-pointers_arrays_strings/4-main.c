2dc3cd78109aba14c276b8ed676c42b7832f9029
dabrabgellak <barbaracalle10i3@gmail.com>
2019-07-02 18:15:15 +0000

    prints a string

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ */
+
+void print_rev(char *s)
+{
+	int in;
+
+	int count(char *str)
+	{
+		for (in = 0; str[in] != '\0'; str++)
+		{}
+		return (i);
+	}
+
+	n = count(str);
+	for (in = n-1; in >= 0; 1--)
+	{
+		_putchar(str[in]);
+	}
+}
