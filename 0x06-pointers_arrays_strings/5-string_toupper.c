92ac0932b0a4fcddb8d25c5a6cb61219663a2eab
dabrabgellak <barbaracalle10i3@gmail.com>
2019-07-04 22:38:48 +0000

    from lowercase to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,12 +1,24 @@
 #include "holberton.h"
 
 /**
- * string_toupper - .
- * @*: 
- *
+ * string_toupper - From lowercase to uppercase.
+ * @min: String.
+ * Return: Min.
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *min)
 {
+	int pi; /**posiscion inicial*/
 
+	for (pi = 0; min[pi] != '\0'; pi++)
+	/**Posicion inicial que sea diferente a null y se sume*/
+	{
+		if (min[pi] >= 97 && min[pi] <= 122)
+	/**Si posicion inicial es mayor o igual a 97 y menor o igual a 122 so*/
+		{
+			min[pi] = min[pi] - 32;
+	/**Posicion inicial es igual a si misma diferencia de 32*/
+		}
+	}
+	return (min);/**Retorna el valor*/
 }
