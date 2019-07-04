cb92face66823bb8cb34397061cf4bcec3459e2e
dabrabgellak <barbaracalle10i3@gmail.com>
2019-07-04 21:55:09 +0000

    betty alright

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * reverse_array - Reverse content of array. 
+ * reverse_array - Reverse content of array.
  * @a: Array.
  * @n: Number of the content.
  */
@@ -14,8 +14,8 @@ void reverse_array(int *a, int n)
 	for (swap = 0; swap < n; swap++)
 	/**loop para saber la posicion inicial*/
 	{
-		poss = a[n-1];/**Buffer equals to posicion final*/
-		a[n-1] = a[swap];/**Posicion final -'\0' equals to posicion inial*/
+		poss = a[n - 1];/**Buffer equals to posicion final*/
+		a[n - 1] = a[swap];/**Posicion final -'\0' equals to posicion inial*/
 		a[swap] = poss;/**Posicion inicial equals to buffer*/
 		n--;/**Decrementa porque es un reverse*/
 	}
