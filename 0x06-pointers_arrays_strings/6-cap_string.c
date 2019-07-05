ebbe0cfed804e405154139ccb55b42a78093c6e2
dabrabgellak <barbaracalle10i3@gmail.com>
2019-07-05 00:15:21 +0000

    capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * cap_string - Capitalizes all words.
+ * @cap: Capitalize.
+ * Return: cap.
+ */
+
+char *cap_string(char *cap)
+{
+	int pi;
+
+	if (cap[0] >= 97 && cap[0] <= 122)
+	/**cap en posicion 0 es >= que a y <= que z so*/
+	{
+		cap[0] = cap[0] - 32;
+	/**
+	* cap en posicion 0 equals to cap minus 32
+	* meaning that it will capitalize the minusculas if they are
+	* at the beggining
+	*/
+	}
+	for (pi = 1; cap[pi] != '\0'; pi++)
+	/**pi igual a 1 bc at the if it's specified from zero */
+	{
+		if ((cap[pi - 1] == ' ' || cap[pi - 1] == '\t'
+		 || cap[pi - 1] == '\n' || cap[pi - 1] == ','
+		|| cap[pi - 1] ==  ';' || cap[pi - 1] == '?'
+		|| cap[pi - 1] == '"' || cap[pi - 1] == '('
+		|| cap[pi - 1] == ')' || cap[pi - 1] == '{' || cap[pi - 1] == '}') &&
+		 (cap[pi] > 'a' && cap[pi] < 'z'))
+		{
+			cap[pi] = cap[pi] - 32;
+		}
+	}
+	return (cap);
+}
