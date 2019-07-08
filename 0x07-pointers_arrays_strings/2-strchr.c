04346cb8ed7481e6868b54c5a4fc85d251a9c706
dabrabgellak <barbaracalle10i3@gmail.com>
2019-07-08 21:02:46 +0000

    locates the char

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * _strchr - Locates the char in a string.
+ * @s: string.
+ * @c: char.
+ * Return: Two returns.
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+	/**Recorrera el string y evaluara */
+		if (*s == c)
+		{
+		/**si en s hay un char*/
+			return (s);
+		/**si lo hay, retorne lo que hay despues del char*/
+		}
+		s++; /**e incremente*/
+	}
+	if (*s == c)
+	{
+                /**en pointer s hay un char*/
+		return (s);
+                /**retornelo*/
+	}
+
+	else
+	{
+		/**sino retorne char null*/
+		return ('\0');
+	}	
+}
