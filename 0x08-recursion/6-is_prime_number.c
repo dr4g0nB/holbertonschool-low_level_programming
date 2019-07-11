b7e7efd4f2037b82aa73a260b0ce7f71c083022f
dabrabgellak <barbaracalle10i3@gmail.com>
2019-07-11 23:02:55 +0000

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,12 +1,40 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * evaluate - Auxiliar function.
+ * @n: number.
+ * @num: aux var.
+ * Return: 3 options.
+*/
+
+int evaluate(int n, int num)
+{
+	if (n == num)
+	{
+		return (1);
+	}
+	if (n % num == 0 || n <= 1)
+	{
+		return (0);
+	}
+	return (evaluate(n, num + 1));
+}
+
+/**
+ * is_prime_number - Prints prime number.
+ * @n: Number.
+ * Return: Prime number.
  */
 
 int is_prime_number(int n)
 {
+	int prime;
 
+	prime = 2;
+
+	if (n == 1)
+	{
+		return (0);
+	}
+	return (evaluate(n, prime));
 }
