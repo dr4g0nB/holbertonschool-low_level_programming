3c1a8b93aaef63ba2341f217a74426d0a9cab8bc
dabrabgellak <barbaracalle10i3@gmail.com>
2019-07-11 23:44:33 +0000

    palindrom words

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,13 +1,59 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * len - function that gets the length.
+ * @s: string.
+ * Return: 2 option.
+ */
+
+int len(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+	return (1 + len(s));
+}
+
+/**
+ * compare - Function that compares le len && s
+ * @s: string.
+ * @len: length.
+ * Return: 3 options.
+*/
+
+int compare(char *s, int len)
+{
+	if (len <= 1)
+	{
+		return (1);
+	}
+	else if (*s == *(s + len - 1))
+	{
+		return (compare(s + 1, len - 2));
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+/**
+ * is_palindrome - Function.
+ * @s: string.
+ * Return: 2 options.
  */
 
 int is_palindrome(char *s)
 {
+	int f;
 
+	f = len(s);
 
+	if (f <= 1)
+	{
+		return (1);
+	}
+	return (compare(s, f));
 }
