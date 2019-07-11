ac8a7272069c170ca17b85f4303bfad8bd5423bf
dabrabgellak <barbaracalle10i3@gmail.com>
2019-07-11 18:09:46 +0000

    ! of any number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * factorial - Returns factorial of n.
+ * @n: given number.
+ * Return: 3 options.
+ */
+
+int factorial(int n)
+{
+	if (n < 0)/**if n is less than zero*/
+	{
+		return(-1);/**return -1, indicates error*/
+	}
+	if (n == 0)/**if n equals 0*/
+	{
+		return(1);/**return 1 cause !0 = 1*/
+	}
+	/**If none of the above then return the !n*/
+	return (n * factorial(n-1));
+}
