61a92c157d50c3d072ef724938ada6d12992aaca
dabrabgellak <barbaracalle10i3@gmail.com>
2019-07-15 01:49:20 +0000

    betty 1.0

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	int r, sum; 
+	int r, sum;
 	/**int d;*/
 
 	/**d = '97';*/
@@ -19,17 +19,21 @@ int main(int argc, char *argv[])
 
 	if (argc > 1)
 	{
-		for (sum = 0; sum < argc; sum++){
-		r = r + atoi(argv[sum]);}
+		for (sum = 0; sum < argc; sum++)
+		{
+		r = r + atoi(argv[sum]);
+		}
 		printf("%d\n", r);
 	}
 	else
 	{
-		printf("0\n");	
+		printf("0\n");
 	}
-	/**if (d >= '97' && d <= '122')
-	{
-		printf("Error\n");
-	}*/	
+	/**
+	* if (d >= '97' && d <= '122')
+	* {
+	*	printf("Error\n");
+	* }
+	*/
 	return (1);
 }
