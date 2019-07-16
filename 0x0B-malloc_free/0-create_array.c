958060e11fe84206136ed942516db3d9b14f4963
dabrabgellak <barbaracalle10i3@gmail.com>
2019-07-16 21:03:46 +0000

    Crestes the arrayy

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * create_array - Creates an array of strings.
+ * @size: size of chars.
+ * @c: Chars.
+ * Return: NULL or a pointer.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int cr; /***/
+	char *p; /***/
+
+	p = malloc(size * sizeof(char));
+	
+	if (size == 0)
+	{
+		return("/0");
+	}
+	else
+	{
+		for (cr = 0; cr < size; cr++)
+		{
+			p[cr] = c;
+		}
+	p[cr] = '\0';
+	return (p);
+	free(p);
+	}
+}
