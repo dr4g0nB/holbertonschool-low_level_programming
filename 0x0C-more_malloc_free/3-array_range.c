87d7d9a7f3721277975fa2022f1af88dfa183db1
dabrabgellak <barbaracalle10i3@gmail.com>
2019-07-18 22:35:59 +0000

    Creates an array

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -10,5 +11,23 @@
 
 int *array_range(int min, int max)
 {
+	int *arrin;
+	int full;
 
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	arrin = malloc(sizeof(int) * (max - min + 1));
+	if (arrin == NULL)
+	{
+		return (NULL);
+	}
+	for (full = 0;  min <= max; full++)
+	{
+		arrin[full] = min;
+		min++;
+	}
+	return (arrin);
 }
