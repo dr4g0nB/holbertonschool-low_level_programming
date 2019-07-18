6f876d66ef77c3030d04a7612a3e70de83a55fe9
dabrabgellak <barbaracalle10i3@gmail.com>
2019-07-18 21:45:20 +0000

    Changed null to zero

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *alloc;
 	unsigned int rein;
 
-	if (nmemb == NULL || size == NULL)
+	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
