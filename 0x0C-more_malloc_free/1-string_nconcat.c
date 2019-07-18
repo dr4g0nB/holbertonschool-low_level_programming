62a79598bb15d5a67b1b12a72221a9e049d4fb2b
dabrabgellak <barbaracalle10i3@gmail.com>
2019-07-18 20:27:29 +0000

    With comments

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,32 +19,35 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	empty = "";
 
-	if (s1 == NULL)
+	if (s1 == NULL)/** Adds empty string to s1*/
 	{
 		s1 = empty;
 	}
 	for (len = 0; s1[len] != '\0'; len++)
-	;
+	;/** Length of s1*/
 
-	if (s2 == NULL)
+	if (s2 == NULL)/** and s2*/
 	{
 		s2 = empty;
 	}
 	for (leng = 0; s2[leng] != '\0'; leng++)
-	;
+	;/** and s2*/
 
 	s3 = malloc((len + (n * sizeof(*s2) + 1)) * sizeof(*s3));
+	/** Reserve bytes for what's coming*/
 	if (s3 == NULL)
 	{
 		return (NULL);
 	}
-
+	/** In case malloc doesn't work*/
 	for (uno = 0; uno < len; uno++)
 	{
+	/**Concatenates s1*/
 		s3[uno] = s1[uno];
 	}
 	for (dos = 0; dos < n && s2[dos] != '\0'; dos++, uno++)
 	{
+	/** Concatenates s2 with a condition*/
 		s3[uno] = s2[dos];
 	}
 	s3[uno] = '\0';
