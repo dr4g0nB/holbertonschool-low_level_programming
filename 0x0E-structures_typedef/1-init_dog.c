97855694d31fbcc692f3b68aa8253fdec5da0f93
dabrabgellak <barbaracalle10i3@gmail.com>
2019-07-22 22:46:31 +0000

    init dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -16,8 +16,10 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	/**Initializing the variables of the struct*/
 	if (d == NULL)
 	{}
-
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
