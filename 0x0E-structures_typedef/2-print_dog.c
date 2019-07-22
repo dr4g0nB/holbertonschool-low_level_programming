4b11cb3e11c9105acb2cbf92584b0c1639a8fbde
dabrabgellak <barbaracalle10i3@gmail.com>
2019-07-22 22:25:49 +0000

    With conditions

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,14 +13,22 @@ void print_dog(struct dog *d)
 	/**d = malloc(sizeof());*/
 	if (d == NULL)
 	{}
-
-	/**
-	 *	if ( == NULL)
-	 *{
-	 *	printf("(nil)");
-	 *}
-	 */
+	 
+	if (d->name == NULL)
+	{
+	 	printf("(nil)");
+	}
 	printf("Name: %s\n", d->name);
+
+	if (d->age == '\0')
+	{
+		printf("(nil)");
+	}
 	printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+	{
+		printf("(nil)");
+	}
 	printf("Owner: %s\n", d->owner);
 }
