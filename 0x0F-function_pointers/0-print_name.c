ec43525af9d2c70e8baa5f9930f000db8a5a18c3
dabrabgellak <barbaracalle10i3@gmail.com>
2019-07-24 20:46:26 +0000

    Prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - Prints a name.
+ * @name: Name to be printed .
+ * 
+ *
+ * Return: 0.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	/**Create a function that would receive whats on function f*/
+	void (*print)(char *);
+
+	/**Check if is empty*/
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+
+	/**Giving the address of f to print*/
+	print = f;
+	/**Printing name that the function print received*/
+	print(name);
+}
