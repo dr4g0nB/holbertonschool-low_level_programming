e0b074a3356a87943fedb6be4090761a14a5b102
dabrabgellak <barbaracalle10i3@gmail.com>
2019-07-25 21:47:42 +0000

    Functions with their operations

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,22 +1,59 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Function that will add two int
+ * @a: argv[1].
+ * @b: argv[3].
+ # Return: a + b.
+ */
+
 int op_add(int a, int b)
 {
 	return(a + b);
 }
+
+/**
+ * op_sub - Function that will sub two int
+ * @a: argv[1].
+ * @b: argv[3].
+ # Return: a - b.
+ */
 
 int op_sub(int a, int b)
 {
 	return(a-b);
 }
 
+/**
+ * op_mul - Function that will multiply two int
+ * @a: argv[1].
+ * @b: argv[3].
+ # Return: a * b.
+ */
+
 int op_mul(int a, int b)
 {
 	return(a * b);
 }
 
+/**
+ * op_div - Function that will divided two int
+ * @a: argv[1].
+ * @b: argv[3].
+ # Return: a / b.
+ */
+
 int op_div(int a, int b)
 {
 	return(a / b);
 }
+
+/**
+ * op_mod - Function that give mod of two int
+ * @a: argv[1].
+ * @b: argv[3].
+ # Return: a % b.
+ */
 
 int op_mod(int a, int b)
 {
