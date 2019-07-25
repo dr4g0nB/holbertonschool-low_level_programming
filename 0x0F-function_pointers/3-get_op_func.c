16548a90dec666b8d39c618a6581c0cf30449362
dabrabgellak <barbaracalle10i3@gmail.com>
2019-07-25 23:44:13 +0000

    Function that selects the rigth function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,7 @@
-#define
-
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * get_op_func - Function to perfom operation.
  * @s: Operator passed as argument.
@@ -18,9 +20,14 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i;
 
-	if (s == + || s == - || s == * || s == / || s == %)
+	i = 0;
+	while (i < 5)
 	{
-		return(NULL);
+		if (strcmp(s, ops[i].op) == 0)
+		{
+			return(ops[i].f);
+		}
+	i++;
 	}
-	
+	return(NULL);
 }
