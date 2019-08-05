77c9418a49a751518cd5b2d7422f491b0a10096a
dabrabgellak <barbaracalle10i3@gmail.com>
2019-08-05 18:55:50 +0000

    Returns the number of nodes

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - Returns the number of elements in a list.
+ * @h: pointer.
+ * Return: Number of nodes.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t len;
+
+	while (h !=  '\0')
+	{
+		h = h->next;
+		len++;
+	}
+	return (len);
+}
