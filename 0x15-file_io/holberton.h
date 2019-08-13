a43e1a1ebefd22d49546f8e2cf0cca55ed3990eb
dabrabgellak <barbaracalle10i3@gmail.com>
2019-08-13 23:22:14 +0000

    With libraries

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,5 +1,9 @@
 #ifndef _HOLBERTON_H
 #define _HOLBERTON_H
+#include <stddef.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
