c2168103f1ea1e28b0eff1c1ecc6d00880d1800a
dabrabgellak <barbaracalle10i3@gmail.com>
2019-08-15 00:12:10 +0000

    Cp not work

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* error_read - Error ro read.
+* @file: File.
+*/
+
+void error_read(char *file)
+{
+	dprintf(STDERROR_FILENO, Error : Can't read from file NAME_OF_THE_FILE);
+	exit(98);
+}
+
+/**
+* error_write - Error to write.
+* @file: File.
+*/
+
+void error_write(char *file)
+{
+	dprintf();
+	exit(99);
+}
+
+/**
+* error_close - Error to close
+* @file: File.
+*/
+
+void error_close(char *file)
+{
+	dprintf(STDERRPR_FILENO, Error : Can't close fd FD_VALUE);
+	exit(100);
+}
+
+/**
+* main - Copies from one file to another one.
+* @argc: Counts.
+* @argv: Arguments.
+* Return: 0.
+*/
+
+int main(int argc, char *argv[])
+{
+	int op = 0, en = 0, rd = 0, wr = 0, clop, clen;
+	char buf[1024];
+
+	if (argc != 3)
+		dprintf(STEDERR_FILENO, Usage : cp file_from file_to);
+		exit(97);
+	/** Open origin file */
+	op = open(argv[1], 0_RDONLY);
+	if (op == -1)
+		dprintf(STDERROR_FILENO, Error : Can't read from file NAME_OF_THE_FILE);
+		exit(98);
+	/** Opens destiny file */
+	en = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (en == -1)
+	{
+		dprintf(STDERROR_FILENO, Error : Can't read from file NAME_OF_THE_FILE);
+		exit(98);
+	}
+	/** Read and write and repeat if equal to buf */
+	do {
+		rd = read(op, buf, 1024);
+		if (rd == -1)
+			dprintf(STDERROR_FILENO, Error : Can't read from file NAME_OF_THE_FILE);
+			 exit(98);
+		wr = write(en, buf, rd);
+		if (wr == -1)
+			void error_read(char *file, argv[2]);
+	}
+	while (rd = 1024)
+	{
+		if (wr < rd)
+			void error_write(char *file, argv[2]);
+	}
+	/** Close each file */
+	clop = close(op);
+	if (clop == -1)
+		void error_close(char *file, argv[1]);
+	clen = close(en);
+	if (clen == -1)
+		void error_close(char *file, argv[2]);
+}
