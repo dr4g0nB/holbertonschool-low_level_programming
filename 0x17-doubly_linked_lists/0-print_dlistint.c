60aaf1613dcede78347f99753293eec1a74d6e57
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-13 21:54:24 +0000

    Display the elements

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_dlistint - Prints the elements of linked list.
+ * @h: linked list.
+ * Return: Number of nodes.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int len;
+
+	/* Traverse the list til NULL*/
+	len = 0;
+	while (h != NULL)
+	{
+		if (h->n == '\0')
+		{
+			printf("%d\n", h->n);
+			/* Keep moving to the next one */
+			h = h->next;
+		}
+/*
+*		else
+*		{
+*			printf("%d\n", h->n);
+*			h = h->next;
+*		}
+*/
+		/* Print the value it found on the node*/
+		printf("%d\n", h->n);
+		/* Keep moving to the next one */
+		h = h->next;
+		len++;
+	}
+	return (len);
+}
