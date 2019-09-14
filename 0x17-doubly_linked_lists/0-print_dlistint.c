9e32ab81cf444961258cddf72d9cc5ecc950bcff
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-14 17:43:43 +0000

    Deleted the conditional

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,18 +9,17 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	int len;
+	size_t len;
 
 	/* Traverse the list til NULL*/
 	len = 0;
 	while (h != NULL)
 	{
-		if (h->n == '\0')
-		{
-			printf("%d\n", h->n);
-			/* Keep moving to the next one */
-			h = h->next;
-		}
+		printf("%d\n", h->n);
+		/* Keep moving to the next one */
+		h = h->next;
+
 		len++;
 	}
 	return (len);
+}
