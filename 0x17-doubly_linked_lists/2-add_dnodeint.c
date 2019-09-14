26ec9b6c3baa5301274fac14340c8e212b82d540
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-14 18:09:55 +0000

    doesnt complie

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,25 +11,25 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	 dlistint_t *new_n;
+	dlistint_t *new_n;
 
 	/** Checks if the head is NULL */
-	if (*head == NULL)
+	if (head == '\0')
 		return (NULL);
 
 	/** Allocate memory for the new node */
-	new_n = malloc(sizeof(int));
-	if (new_n == NULL)
+	new_n = malloc(sizeof(dlistint_t));
+	if (new_n == '\0')
+		free(new_n);
 		return (NULL);
 
 	/** Data for new node */
-		new_n->n = n;
-		/** Next is head and previous is NULL */
-		new_n->next = *head;
-		new_n->prev = NULL;
-
-	if (*head != NULL)
-		head = new_n;
+	new_n->n = n;
+	/** Next is head and previous is NULL */
+	new_n->next = *head;
+	*head->prev = new_n;
+	new_n->prev = NULL;
+	*head = new_n;
 
 	/** Returns address of the new node */
 	return (new_n);
