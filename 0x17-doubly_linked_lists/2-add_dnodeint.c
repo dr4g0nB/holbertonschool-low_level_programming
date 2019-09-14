d2f225bf929fd3ce2439d111f759e53cf1da5f16
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-14 19:58:00 +0000

    segmentation fault

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,23 +14,30 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_n;
 
 	/** Checks if the head is NULL */
-	if (head == '\0')
+	if (*head == NULL)
+	{	
 		return (NULL);
+	}
 
 	/** Allocate memory for the new node */
 	new_n = malloc(sizeof(dlistint_t));
-	if (new_n == '\0')
+	if (new_n == NULL)
+	{
 		free(new_n);
 		return (NULL);
+	}
 
-	/** Data for new node */
-	new_n->n = n;
+	/** Data for new node*/
+	new_n->n = n; 
 	/** Next is head and previous is NULL */
-	new_n->next = *head;
-	*head->prev = new_n;
 	new_n->prev = NULL;
+	new_n->next = *head;
+	if (head != NULL)
+	{
+		(*head)->prev = new_n;
+	}
 	*head = new_n;
-
+	
 	/** Returns address of the new node */
 	return (new_n);
 }
