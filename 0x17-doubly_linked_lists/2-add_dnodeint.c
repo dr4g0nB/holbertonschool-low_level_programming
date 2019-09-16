7121308b5441338fe179b56e36290cece5d00a4c
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-16 02:19:02 +0000

    Doesnt compile

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_n;
 
 	/** Checks if the head is NULL */
-	if (*head == NULL)
+	if (head == NULL)
 	{
 		return (NULL);
 	}
@@ -30,9 +30,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	/** Data for new node*/
 	new_n->n = n;
 	/** Next is head and previous is NULL */
-	new_n->prev = NULL;
+	/**new_n->prev = NULL;*/
 	new_n->next = *head;
-	if (head != NULL)
+	new_n->prev = NULL;
+
+	if (*head != NULL)
 	{
 		(*head)->prev = new_n;
 	}
