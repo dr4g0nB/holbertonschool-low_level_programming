18fa6cd5e5eff118e7d271b4d6fcc4bdcad51a97
dabrabgellak <barbaracalle10i3@gmail.com>
2019-10-30 17:56:07 +0000

    More conditionals

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -46,37 +46,43 @@ void check(listint_t *now, listint_t **list)
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *now = NULL, *move = NULL, *idx = NULL;
+	listint_t *now = NULL, *move = NULL, *head = NULL, *idx = NULL;
 
-	if (list == NULL || *list == NULL ||
-		(*list)->next == NULL || (*list)->next->next == NULL)
+	if (list == NULL || *list == NULL)
 		return;
-
 	move = *list;
 	idx = *list;
 	now = *list;
-
 	while (move != NULL)
 	{
-		if (move == idx)
+		head = *list;
+		if (move == head)
 			move = move->next;
 		else if (move != NULL && move->n < move->prev->n)
 		{
 			now = move;
 			move = move->next;
-			now->prev->next = now->next;
+			now->next = idx;
+			idx->next = move;
+			now->prev = idx->prev;
+			if (idx->prev)
+				idx->prev->next = now;
+			idx->prev = now;
 			if (move)
-				now->next->prev = now->prev;
-			now->prev = now->prev->prev;
-			now->next = now->prev->next;
-			now->prev->next = now;
-			now->next->prev = now;
+				move->prev = idx;
+			if (head == idx)
+				*list = now;
+			else if (head != now)
+				*list = head;
+			else
+				*list = now;
 			print_list(*list);
 			check(now, list);
 		}
 		else
 		{
 			move = move->next;
+			idx = idx->next;
 		}
 	}
 }
