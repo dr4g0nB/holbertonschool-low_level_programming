e69089589366a8bee07283904ace7d9bd4a084f5
dabrabgellak <barbaracalle10i3@gmail.com>
2020-01-15 19:17:20 +0000

    better looking header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,8 @@
-#ifndef _SEARCH_ALGOS_H
-#define _SEARCH_ALGOS_H
+#ifndef SEARCH_ALGOS_H_
+#define SEARCH_ALGOS_H_
+
 #include <stdio.h>
+#include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
